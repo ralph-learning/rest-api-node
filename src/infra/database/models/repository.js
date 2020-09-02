5169bfc10d67651c0c999e12eba71c7f8c148f12
Ralph Effting <ralfting@gmail.com>
2020-09-02 16:38:22 -0300

    Fix uuid generate issue

--- src/infra/database/models/repository.js
+++ src/infra/database/models/repository.js
@@ -1,7 +1,15 @@
 "use strict";
 
+const { Sequelize } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   const repository = sequelize.define("repository", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: DataTypes.STRING,
     fullName: DataTypes.STRING,
     descrition: DataTypes.TEXT,
